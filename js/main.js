9ef798f1b43bf914c7906bc933a14e30fa768a7c
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2023-09-10 18:00:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let clos = document.querySelector('.btn.btn-secondary');
+let exit = document.querySelector('.btn-close');
+let goTo = document.querySelector('.go-to');
+let modal = document.querySelector('.modal');
+console.log(clos)
+clos.onclick = ()=>{
+modal.style.display = 'none'
+}
+exit.onclick = ()=>{
+modal.style.display = 'none'
+}
+goTo.onclick = ()=>{
+modal.style.display = 'none'
+}
+

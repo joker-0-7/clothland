4a25897cf51b69993616391e52907adf4a060a8d
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2023-09-15 20:02:21 +0300

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,44 +1,13 @@
-(function () {
-	const second = 1000,
-		  minute = second * 60,
-		  hour = minute * 60,
-		  day = hour * 24;
-  
-	//I'm adding this section so I don't have to keep updating this pen every year :-)
-	//remove this if you don't need it
-	let today = new Date(),
-		dd = String(today.getDate()).padStart(2, "0"),
-		mm = String(today.getMonth() + 1).padStart(2, "0"),
-		yyyy = today.getFullYear(),
-		nextYear = yyyy + 1,
-		dayMonth = "09/30/",
-		birthday = dayMonth + yyyy;
-	
-	today = mm + "/" + dd + "/" + yyyy;
-	if (today > birthday) {
-	  birthday = dayMonth + nextYear;
+setInterval(function time(){
+	var d = new Date();
+	var hours = 24 - d.getHours();
+	var min = 60 - d.getMinutes();
+	if((min + '').length == 1){
+	  min = '0' + min;
 	}
-	//end
-	
-	const countDown = new Date(birthday).getTime(),
-		x = setInterval(function() {    
-  
-		  const now = new Date().getTime(),
-				distance = countDown - now;
-  
-		  document.getElementById("days").innerText = Math.floor(distance / (day)),
-			document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-			document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-			document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-  
-		  //do something later when date is reached
-		  if (distance < 0) {
-			document.getElementById("headline").innerText = "It's my birthday!";
-			document.getElementById("countdown").style.display = "none";
-			document.getElementById("content").style.display = "block";
-			clearInterval(x);
-		  }
-		  //seconds
-		}, 0)
-	}());
-
+	var sec = 60 - d.getSeconds();
+	if((sec + '').length == 1){
+		  sec = '0' + sec;
+	}
+	document.querySelector('#the-24h-countdown p').innerHTML = '<span>'+hours+'</span><span class="min">'+min+'<br></span><span class="seg">'+sec+'</span>'
+  }, 1000);
